441396730aa63eb4abfa155ee2498049aac53c34
Ulrik Sandborg-Petersen <ulrikp@scripturesys.com>
2018-05-07 21:10:07 +0200

    Better tokenization.

--- xml2mql/latin_tokenizer.py
+++ xml2mql/latin_tokenizer.py
@@ -13,7 +13,9 @@
 #
 token_split_chars = " \n\r\t-"
 
-state_between = 0
+token_non_surface_chars = token_split_chars + ".,;:?\""
+
+state_after = 0
 state_in = 1
 
 def tokenize_string(instring):
@@ -22,27 +24,34 @@ def tokenize_string(instring):
 
     tmp_list = []
 
-    state = state_between
+    if len(instring) == 0:
+        return []
+    if instring[0] in token_split_chars:
+        state = state_after
+    else:
+        state = state_in
 
     start = 0
     for index in range(0, len(instring)):
         c = instring[index]
         if c in token_split_chars:
-            if state == state_between:
+            if state == state_after:
                 pass
             elif state == state_in:
-                tmp_str = instring[start:index]
-                start = index
-                tmp_list.append(tmp_str)
+                state = state_after
             else:
                 assert False, "Unhandled state: %s\n" % state
         else:
             if state == state_in:
                 pass
-            elif state == state_between:
-                pass
+            elif state == state_after:
+                tmp_str = instring[start:index]
+                start = index
+                tmp_list.append(tmp_str)
+                state = state_in
             else:
                 assert False, "Unhandled state: %s\n" % state
+
 
     if start < len(instring)-1:
         tmp_str = instring[start:]
@@ -61,7 +70,7 @@ def tokenize_string(instring):
         suffix_list = []
         for index in range(0, len(tmp_str)):
             c = tmp_str[index]
-            if c in token_split_chars:
+            if c in token_non_surface_chars:
                 if state == st_prefix:
                     prefix_list.append(c)
                 elif state == st_surface:
@@ -88,6 +97,9 @@ def tokenize_string(instring):
 
         result_list.append((prefix, surface, suffix))
 
+    print("UP200: " + repr(result_list))
+        
+        
     return result_list
         
     
